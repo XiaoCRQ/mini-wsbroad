58df9243cdedf16797fa12cbc5b8959e18e7b194
XiaoCRQ <xiaocrq001@gmail.com>
2026-03-04 01:21:01 +0800

    1. 更新actions 2. 更新cmake

--- compiling.sh
+++ compiling.sh
@@ -8,7 +8,6 @@ mkdir bin
 
 SRC="main.cpp"
 LINUX_OUT="bin/mini-wsbroad"
-WIN_OUT="bin/mini-wsbroad.exe"
 
 start_time=$(date +%s)
 
@@ -35,23 +34,3 @@ spinner $PID
 wait $PID
 
 printf "\r[Info] ✔ Linux build finished → %s\n" "$LINUX_OUT"
-
-# echo "[Info] Building Windows binary..."
-#
-# if command -v x86_64-w64-mingw32-g++ >/dev/null 2>&1; then
-#   x86_64-w64-mingw32-g++ $CXXFLAGS \
-#     -static -lws2_32 -lmswsock \
-#     "$SRC" -o "$WIN_OUT" &
-#   PID=$!
-#   spinner $PID
-#   wait $PID
-#
-#   printf "\r[Info] ✔ Windows build finished → %s\n" "$WIN_OUT"
-# else
-#   echo "[Warn] mingw not found. Skipping Windows build."
-# fi
-#
-# end_time=$(date +%s)
-# elapsed=$((end_time - start_time))
-#
-# echo "[Info] All done in ${elapsed}s"
